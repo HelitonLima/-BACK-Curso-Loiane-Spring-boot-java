2c5b0dbd32ffdfc66cf97e8f00a5401f5adff9cf
Heliton Martins de Lima <heliton.lima@kron.digital>
2026-04-20 17:42:09 -0300

    CRUD Angular + Spring | 26: API Spring: Buscar por ID

--- src/main/java/com/loiane/controller/CourseController.java
+++ src/main/java/com/loiane/controller/CourseController.java
@@ -3,7 +3,9 @@ package com.loiane.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +32,11 @@ public class CourseController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getById(@PathVariable Long id) {
+        return courseRepository.findById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
