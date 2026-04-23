668bc4e91c2f76b261f3c264a9f23530d3838243
Heliton Martins de Lima <heliton.lima@kron.digital>
2026-04-22 21:48:38 -0300

    CRUD Angular + Spring | 30: Atualizar Curso: API Spring HTTP PUT

--- src/main/java/com/loiane/controller/CourseController.java
+++ src/main/java/com/loiane/controller/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,5 +39,15 @@ public class CourseController {
     public ResponseEntity<Course> getById(@PathVariable Long id) {
         return courseRepository.findById(id).map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, @RequestBody Course course) {
+        return courseRepository.findById(id).map(existingCourse -> {
+            existingCourse.setName(course.getName());
+            existingCourse.setCategory(course.getCategory());
+            Course updatedCourse = courseRepository.save(existingCourse);
+            return ResponseEntity.ok().body(updatedCourse);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
