1a41ac4efd15267a327e4091d610d055ae458fa0
Heliton Martins de Lima <heliton.lima@kron.digital>
2026-04-24 15:14:30 -0300

    CRUD Angular + Spring | 31: Remover Curso: API Spring HTTP DELETE

--- src/main/java/com/loiane/controller/CourseController.java
+++ src/main/java/com/loiane/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,14 @@ public class CourseController {
             existingCourse.setCategory(course.getCategory());
             Course updatedCourse = courseRepository.save(existingCourse);
             return ResponseEntity.ok().body(updatedCourse);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id).map(existingCourse -> {
+            courseRepository.delete(existingCourse);
+            return ResponseEntity.noContent().<Void>build();
         }).orElse(ResponseEntity.notFound().build());
     }
 }
