e830e11d8df55c0fd62b4a8ffd133d32422be328
Heliton Martins de Lima <heliton.lima@kron.digital>
2026-04-30 09:10:32 -0300

    CRUD Angular + Spring | 35: Validações da API com Java Bean Validation

--- src/main/java/com/loiane/model/Course.java
+++ src/main/java/com/loiane/model/Course.java
@@ -1,10 +1,16 @@
 package com.loiane.model;
 
+
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -14,9 +20,15 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank
+    @NotNull
+    @Length(min= 5, max = 200)
     @Column(length = 200, nullable = false)
     private String name;
     
+    @NotNull
+    @Length(max = 20)
+    @Pattern(regexp = "BACK-END|FRONT-END")
     @Column(length = 20, nullable = false)
     private String category;
 }
