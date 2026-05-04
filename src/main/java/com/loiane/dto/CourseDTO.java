288d6e6c62051412d1c48f8b42ab65c1be65d0c4
Heliton Martins de Lima <heliton.lima@kron.digital>
2026-05-04 13:32:17 -0300

    CRUD Angular + Spring | 46: Curso-Aulas: Listando Aulas

--- src/main/java/com/loiane/dto/CourseDTO.java
+++ src/main/java/com/loiane/dto/CourseDTO.java
@@ -1,6 +1,10 @@
 package com.loiane.dto;
 
+import java.util.List;
+
 import org.hibernate.validator.constraints.Length;
+
+import com.loiane.model.Lesson;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,5 +13,6 @@ import jakarta.validation.constraints.Pattern;
 public record CourseDTO(
     Long id,
     @NotBlank @NotNull @Length(min= 5, max = 200) String name,
-    @NotNull @Length(max = 20) @Pattern(regexp = "Back-end|Front-end") String category) {
+    @NotNull @Length(max = 20) @Pattern(regexp = "Back-end|Front-end") String category,
+    List<Lesson> lessons) {
 }
--- src/main/java/com/loiane/dto/mapper/CouseMapper.java
+++ src/main/java/com/loiane/dto/mapper/CouseMapper.java
@@ -14,7 +14,12 @@ public class CouseMapper {
             return null;
         }
 
-        return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue());
+        return new CourseDTO(
+            course.getId(), 
+            course.getName(), 
+            course.getCategory().getValue(),
+            course.getLessons()
+        );
     }
     
     public Course toEntity(CourseDTO courseDTO) {
