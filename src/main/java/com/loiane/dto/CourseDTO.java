2adb3f6b9167f07f8a60cc7ef55dd425b09e60dd
Heliton Martins de Lima <heliton.lima@kron.digital>
2026-05-12 08:29:15 -0300

    CRUD Angular + Spring | 57: Validando Enums com Java Bean Validations

--- src/main/java/com/loiane/dto/CourseDTO.java
+++ src/main/java/com/loiane/dto/CourseDTO.java
@@ -1,18 +1,20 @@
 package com.loiane.dto;
 
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.hibernate.validator.constraints.Length;
+
+import com.loiane.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
     Long id,
     @NotBlank @NotNull @Length(min= 5, max = 200) String name,
-    @NotNull @Length(max = 20) @Pattern(regexp = "Back-end|Front-end") String category,
+    @NotNull @Length(max = 20) @ValueOfEnum(enumClass = Category.class) String category,
     @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
